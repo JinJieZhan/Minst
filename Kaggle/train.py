0d816754710931ba9430e87a8d1bea522a20e477
JinJieZhan <zhaojinjie@aliyun.com>
2024-09-07 11:58:53 +0800

    Update train.py

--- Kaggle/train.py
+++ Kaggle/train.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_classes', type=int, default=10)
     parser.add_argument('--epochs', type=int, default=5)
     parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--data-path', type=str, default="./data/MNIST")
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
 
